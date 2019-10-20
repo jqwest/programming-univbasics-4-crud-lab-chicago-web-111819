1c32ba95a2a42b2bd28052e175aecd052415ab98
john L <johnleavell@gmail.com>
2019-10-20 03:04:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  famous_cats = ["wow", "tony", "grumpy", "arrays!"]
+  famous_cats[0]
 end
 
 def retrieve_first_element_from_array(array)
