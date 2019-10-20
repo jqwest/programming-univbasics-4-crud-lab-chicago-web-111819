ca0a4eb5c04cb769faee4a290aeebde462b33655
john L <johnleavell@gmail.com>
2019-10-20 03:02:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,7 +42,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
+  new_array = ["ford", "honda", "acura"]
 
+  new_array[2]
 end
 
 def update_element_from_index(array, index_number, element)
