604de6ae2ca88686a71ab341f5c789be005d017d
john L <johnleavell@gmail.com>
2019-10-20 03:04:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   famous_cats = ["am", "tony", "grumpy", "arrays!"]
-  famous_cats[0]
+  p famous_cats[0]
 end
 
 def retrieve_first_element_from_array(array)
