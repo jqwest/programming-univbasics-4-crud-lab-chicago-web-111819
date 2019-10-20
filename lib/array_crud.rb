0cfea7f3a4bb8bfeb8cc8cf8d6c9455592bc5abd
john L <johnleavell@gmail.com>
2019-10-20 02:55:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   color_of_doors = ["blue", "orange", "black"]
-  new_color = "pink"
+  new_color = "wow"
   color_of_doors.unshift(new_color)
 
 end
