79332a8b6a2595efc304049c60624751baa1c3a8
john L <johnleavell@gmail.com>
2019-10-20 03:04:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  famous_cats = ["wow", "tony", "grumpy", "arrays!"]
+  famous_cats = ["am", "tony", "grumpy", "arrays!"]
   famous_cats[0]
 end
 
